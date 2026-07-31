2016eb5cb7eb1fe69b69db75f8079b70cbeff9bd
diaz <lazuardyas17001@mail.unpad.ac.id>
2026-07-31 17:03:41 +0700

    refactor pakai React dan shadcn

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import { Button } from "./components/ui/button";
+import './App.css'    
+
+function App() {
+  return (
+    <>
+    <h1 className="text-5xl">Hello! aRtyjG</h1>
+    <Button>Testing Buttonz</Button>
+    </>
+  )
+}
+
+export default App
